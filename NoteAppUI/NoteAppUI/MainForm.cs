522324fafb7f551132c06ea28b6fe548a2e56540
Maksershov98 <Maksevov@gmail.com>
2019-12-02 22:13:30 +0700

    Разработана логика приложения, классы: Номер телефона, Контакты, Проект, Менеджер проекта

--- NoteAppUI/NoteAppUI/MainForm.cs
+++ NoteAppUI/NoteAppUI/MainForm.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace NoteAppUI
 {
-    public partial class MainForm : Form
+    public partial class Form1 : Form//Комментарий
     {
         public MainForm()
         {
